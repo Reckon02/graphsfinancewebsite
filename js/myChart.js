de2d42d2af833436bde9cefe1f5db1f698c525ca
Simon <simon.rapanelli@gmail.com>
2022-08-01 13:22:18 +0200

    Improved things!

--- js/myChart.js
+++ js/myChart.js
@@ -14,13 +14,19 @@ const data = {
   labels: labels,
   datasets: [
     {
-      label: "My First graphic",
+      label: "TSLA",
       backgroundColor: "rgb(255, 99, 132)",
       borderColor: "rgb(255, 99, 132)",
       data: [0, 10, 5, 2, 40, 30, 45, 20, 60],
     },
+  ],
+};
+
+const data2 = {
+  labels: labels,
+  datasets: [
     {
-      label: "Another Graphic",
+      label: "AAPL",
       backgroundColor: "rgb(19, 209, 235)",
       borderColor: "rgb(19, 209, 235)",
       data: [5, 46, 12, 7, 2, 9, 50, 12, 64],
@@ -34,7 +40,23 @@ const config = {
   data: data,
   options: {},
 };
-const myChart = new Chart(document.getElementById("myChart"), config);
+
+const config2 = {
+  type: "bar",
+  data: data2,
+  options: {},
+};
+let myChart = new Chart(document.getElementById("myChart"), config);
+
+function renderChart(type) {
+  myChart.destroy();
+  if (type.value === "tesla") {
+    myChart = new Chart(document.getElementById("myChart"), config);
+  }
+  if (type.value === "apple") {
+    myChart = new Chart(document.getElementById("myChart"), config2);
+  }
+}
 
 function chartType(type) {
   myChart.config.type = type;
